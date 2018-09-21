bc52849648478ddf82386e7b0321c8afb999d2fd
Слава Иевлев <slavaievlev9@gmail.com>
2018-09-21 20:23:34 +0400

    Added interface for Lab 2

---
+++ WindowsFormsCars/ITransport.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsCars
+{
+    interface ITransport
+    {
+        /// <summary>
+        /// Установка позиции автомобиля
+        /// </summary>
+        /// <param name="x">Координата X</param>
+        /// <param name="y">Координата Y</param>
+        /// <param name="width">Ширина картинки</param>
+        /// <param name="height">Высота картинки</param>
+        void SetPosition(int x, int y, int width, int height);
+
+        /// <summary>
+        /// Изменение направления движения
+        /// </summary>
+        /// <param name="direction">Направление</param>
+        void MoveTransport(Bus.Direction direction);
+
+        /// <summary>
+        /// Отрисовка автомобиля
+        /// </summary>
+        /// <param name="g"></param>
+        void DrawCar(Graphics g);
+    }
+}
